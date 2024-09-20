88da4b09c72f60d34306faad4fcdfc29be35b8c6
dummySatya <dummySatya5@gmail.com>
2024-09-20 11:34:48 +0530

    feat: create a mathLib library containing packages and modules

---
+++ 1/mathLib/complexops.py
@@ -0,0 +1,9 @@
+def mod(a, b):
+    return a % b
+
+def exp(a, b):
+    return a ** b
+
+def add(a, b):
+    print("From lib's module")
+    return a + b
---
+++ 1/mathLib/simpleops/__init__.py
@@ -0,0 +1,4 @@
+# this file is optional from python 3.3 as u can directly import too, but this can be usefull when we want to import only specific modules from a specific package
+from .addsub import *
+from .muldiv import *
+# the dot is for relative import (i.e, search in this current directory)
