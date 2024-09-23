c024d1eab8d1ecf6c30ff5f37d331535078f0149
dummySatya <dummySatya5@gmail.com>
2024-09-23 19:23:18 +0530

    feat: test file

---
+++ 2/test.py
@@ -0,0 +1,11 @@
+
+                      #s
+
+# Implementing foo function
+def foo():
+    print("Hello this is foo")
+
+                                                                                                                           
+  # Implementing bar function
+def bar():
+    print("Hello this is bar")
